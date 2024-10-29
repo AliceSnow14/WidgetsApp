bf75665040909b36608c98671508494ff5a70785
AliceSnow <patricia.aranguiz95@gmail.com>
2024-10-29 02:24:06 -0300

    first commit

---
+++ lib/src/presentation/screens/home_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 149, 226, 236),
+      body: Placeholder(),
+    );
+  }
+}
