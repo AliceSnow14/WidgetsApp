2ead0aa97addb2a95a3249a5a5815ab5d9223383
AliceSnow <patricia.aranguiz95@gmail.com>
2024-10-30 02:59:06 -0300

    creamos el menu

--- lib/src/presentation/screens/home_screen.dart
+++ lib/src/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,12 +11,39 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Widgets Apps"),
       ),
       backgroundColor: const Color.fromARGB(255, 149, 226, 236),
-      body: Center(
-        child: FilledButton(
-          onPressed: () {},
-          child: const Text("press"),
-        ),
-      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final MenuItem menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final MenuItem menuItem;
+  const _CustomListTile({required this.menuItem});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      onTap: () {},
+      leading: Icon(menuItem.icon, color: colors.primary),
+      subtitle: Text(menuItem.subtitle),
+      title: Text(menuItem.title),
     );
   }
 }
